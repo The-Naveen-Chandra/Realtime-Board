ee34e1bab150691e87c9b3c69fe13e9e83028541
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-02-02 20:40:57 +0530

    Dev: empty search, empty favorites, empty board, and empty org components

---
+++ app/(dashboard)/_components/board-list.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { EmptyBoard } from "./empty-board";
+import { EmptyFavorites } from "./empty-favorites";
+import { EmptySearch } from "./empty-search";
+
+interface BoardListProps {
+  orgId: string;
+  query: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+export const BoardList = ({ orgId, query }: BoardListProps) => {
+  const data = []; //TODO: change to API call
+
+  if (!data?.length && query.search) {
+    return <EmptySearch />;
+  }
+
+  if (!data?.length && query.favorites) {
+    return <EmptyFavorites />;
+  }
+
+  if (!data?.length) {
+    return <EmptyBoard />;
+  }
+
+  return <div></div>;
+};
