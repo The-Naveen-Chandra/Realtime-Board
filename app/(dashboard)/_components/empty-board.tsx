5304b0ce6f7e41febb71e613d1f4901d07d8ce50
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-02-04 15:18:30 +0530

    Dev: board creation method

--- app/(dashboard)/_components/empty-board.tsx
+++ app/(dashboard)/_components/empty-board.tsx
@@ -1,7 +1,27 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import Image from "next/image";
+import { useMutation } from "convex/react";
+import { useOrganization } from "@clerk/nextjs";
+
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
 
 export const EmptyBoard = () => {
+  const { organization } = useOrganization();
+  const create = useMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) {
+      return;
+    }
+
+    create({
+      orgId: organization.id,
+      title: "Untitled",
+    });
+  };
+
   return (
     <div className="h-[calc(100%-100px)] flex flex-col items-center justify-center">
       <Image
@@ -11,11 +31,13 @@ export const EmptyBoard = () => {
         width={110}
       />
       <h2 className="text-2xl font-semibold mt-6">Create your first board!</h2>
-      <p className=" text-muted-foreground text-sm mt-2">
+      <p className="text-muted-foreground text-sm mt-2">
         Start by creating a board for your organization.
       </p>
       <div className="mt-6">
-        <Button size={"lg"}>Create board</Button>
+        <Button onClick={onClick} size={"lg"}>
+          Create board
+        </Button>
       </div>
     </div>
   );
