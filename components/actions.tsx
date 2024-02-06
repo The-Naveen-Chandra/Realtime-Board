42e317a9ab9861cf7b6e356a5acf432dd3877f54
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-02-06 17:49:23 +0530

    Dev: actions to copy board link / delete board with confirm modal

---
+++ components/actions.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { toast } from "sonner";
+import { Link2, Trash2 } from "lucide-react";
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+
+import { ConfirmModal } from "@/components/confirm-modal";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+
+interface ActionsProps {
+  children: React.ReactNode;
+  side?: DropdownMenuContentProps["side"];
+  sideOffset?: DropdownMenuContentProps["sideOffset"];
+  id: string;
+  title: string;
+}
+
+export const Actions = ({
+  children,
+  side,
+  sideOffset,
+  id,
+  title,
+}: ActionsProps) => {
+  const { mutate, pending } = useApiMutation(api.board.remove);
+
+  // Function for copying the board link
+  const onCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => toast.success("Link copied"))
+      .catch(() => toast.error("Failed to copy link"));
+  };
+
+  // Function for deleting the board
+  const onDelete = () => {
+    mutate({ id })
+      .then(() => toast.success("Board deleted"))
+      .catch(() => toast.error("Failed to delete board"));
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        onClick={(e) => e.stopPropagation()}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-60"
+      >
+        {/* Copy board link */}
+        <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}>
+          <Link2 className="h-4 w-4 mr-2" />
+          Copy board link
+        </DropdownMenuItem>
+
+        {/* Deleting the board */}
+        <ConfirmModal
+          header={"Delete board?"}
+          description={"This will delete the board and all of its contents."}
+          disabled={pending}
+          onConfirm={onDelete}
+        >
+          <Button
+            className="p-3 cursor-pointer text-sm w-full justify-start font-normal"
+            variant={"ghost"}
+          >
+            <Trash2 className="h-4 w-4 mr-2" />
+            Delete
+          </Button>
+        </ConfirmModal>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
