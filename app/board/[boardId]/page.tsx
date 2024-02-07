14b00be33e8fe38dad7d5a0f1a5b622f4f43daaa
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-02-07 20:32:49 +0530

    Dev: loading for each component

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,5 +1,6 @@
-import { Canvas } from "./_components/canvas";
 import { Room } from "@/components/room";
+import { Canvas } from "./_components/canvas";
+import { Loading } from "./_components/loading";
 
 interface BoardIdPageProps {
   params: {
@@ -9,7 +10,7 @@ interface BoardIdPageProps {
 
 const BoardIdPage = ({ params }: BoardIdPageProps) => {
   return (
-    <Room roomId={params.boardId} fallback={<div>Loading...</div>}>
+    <Room roomId={params.boardId} fallback={<Loading />}>
       <Canvas boardId={params.boardId} />;
     </Room>
   );
