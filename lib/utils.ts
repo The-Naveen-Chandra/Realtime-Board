2c49daaff2b0e09a1616573b80f3f7a061ba1759
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-02-08 19:29:38 +0530

    Dev: Participants component

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,22 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const COLORS = [
+  "#DC2626",
+  "#FBBF24",
+  "#059669",
+  "#3B82F6",
+  "#A78BFA",
+  "#F472B6",
+  "#DB2777",
+  "#F97706",
+  "#7C3AED",
+];
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
 }
