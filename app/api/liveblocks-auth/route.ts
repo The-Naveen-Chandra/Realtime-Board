d14a4b2182e8cd270a9beb66bc3e7b178aa31e0c
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-02-16 18:34:09 +0530

    Fix: fixed some issues

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -7,8 +7,7 @@ import { api } from "@/convex/_generated/api";
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const liveblocks = new Liveblocks({
-  secret:
-    "sk_dev_ooye_El7qWt2d1CU31umpDm1gPrwUvGUz6o4fbS_OBy_HSf23z1L_9m450DGZT-G",
+  secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
 
 export async function POST(request: Request) {
